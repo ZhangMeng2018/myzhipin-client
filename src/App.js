5578123d9951d8fe71c065af6df91ab346f76709
Zhang Meng <970092132@qq.com>
2018-08-11 13:45:51 +0800

    first update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Button} from 'antd-mobile'
+
+class App extends Component {
+  render() {
+    return (
+      <Button type='primary' >React</Button>
+    );
+  }
+}
+
+export default App;
